1d0d4b934c765d8d5b47482d87400a0a3cda4ee2
Jane Le <janehtle@gmail.com>
2025-08-11 00:22:11 -0400

    json file added' git status

---
+++ products/product.js
@@ -0,0 +1,31 @@
+//fetch content in json file and will return a Promise - either a Response object with the content in json file or error if it can't
+fetch("products.json")
+    .then(response => response.json()) //once fetched, response object written in json format, which will turn into usable js materials
+    .then(data => { //use data for..
+        for (const category in data) { //category: discography, fashion, bunini, loop through each category
+        const categoryName = category.toLowerCase(); //category name need to match the ID on HTML
+        const container = document.getElementById(`${categoryName}Products`); //match discographyProducts, fashionProducts, buniniProducts
+
+        if (!container) continue; //continue if container/ID not found in HTML
+
+        data[category].forEach(product => { //iterate through every product in category
+            const productDiv = document.createElement("div"); //will create a new div - productDiv
+            productDiv.classList.add("product"); //adds product class to the div
+
+            //this is what I commented out on my HTML, so JS should display this in its place again
+            productDiv.innerHTML = 
+            `<img src="${product.image}" alt="${product.alt}">
+            <p class="productName">${product.name}</p>
+            <h4 class="price">$${product.price.toFixed(2)}</h4>`;
+            container.appendChild(productDiv); //will take its place in productDiv and display on DOM
+        });
+        }
+
+        console.log(data); //inspected console in the browser, Response object present
+
+    })
+    .catch(error => console.error("Error loading products:", error)); //display error if Response object not found or data not fetched
+
+
+
+
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const playBtn = document.getElementById("playBtn");
 const music = document.getElementById("song");
 
+//add functionality to music button
 playBtn.addEventListener("click", () => {
     if (music.paused) {
         music.play();
